19760a961012834e6c8fb4f99b5cf9c11819b0bd
Linhngo268 <83965979+Linhngo268@users.noreply.github.com>
2022-03-14 12:30:30 +1030

    Update Referee.h

--- Referee.h
+++ Referee.h
@@ -1,21 +1,16 @@
-#ifndef REFEREE_H
-#define REFEREE_H
-#include "human.h"
 #include "Computer.h"
+#include <iostream>
 #include<string>
  
  class Referee{
      
 public:
      
-Referee();
+     Referee();
      
-char refGame(Human& player1, Computer& player2){
-     }
+      char refGame(Human player1, Computer player2);
      
-private:
-char player1;
-char player2;
+ 
 
 
 
